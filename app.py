7f1927332f23272e0c9f6b4f2ab048299d156615
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-01-27 07:49:30 -0500

    app.py

--- app.py
+++ app.py
@@ -392,12 +392,12 @@ table.style = 'Table Grid'
 hdr = table.rows[0].cells
 hdr[0].text, hdr[1].text, hdr[2].text = 'Certificación', 'Cursos', 'Créditos Totales'
     
-    for c in cert_data:
-        if c["Nombre"]:
-                row = table.add_row().cells
-                row[0].text = c["Nombre"]
-                row[1].text = f"{c['Curso 1']}, {c['Curso 2']}"
-                row[2].text = str(c["Créditos 1"] + c["Créditos 2"])
+for c in cert_data:
+    if c["Nombre"]:
+            row = table.add_row().cells
+            row[0].text = c["Nombre"]
+            row[1].text = f"{c['Curso 1']}, {c['Curso 2']}"
+            row[2].text = str(c["Créditos 1"] + c["Créditos 2"])
             
 
 
@@ -423,6 +423,7 @@ hdr[0].text, hdr[1].text, hdr[2].text = 'Certificación', 'Cursos', 'Créditos T
    #     file_name=f"PEP_{denom.replace(' ', '_')}.docx",
     #    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 #)
+
 
 
 
