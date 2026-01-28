472579beac9c7582534a863d47cdb2ac7113a4b0
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-01-28 09:11:17 -0500

    app.py

--- app.py
+++ app.py
@@ -382,16 +382,16 @@ recons_validos = [r for r in recon_data if r.get("Nombre del premio", "").strip(
         
 if recons_validos:
         doc.add_paragraph(
-             f"El Programa de {denom} ha alcanzado importantes logros académicos e institucionales "
-             f"que evidencian su calidad y compromiso con la excelencia. Entre ellos se destacan:"
+            f"El Programa de {denom} ha alcanzado importantes logros académicos e institucionales "
+            f"que evidencian su calidad y compromiso con la excelencia. Entre ellos se destacan:"
          )
-             for r in recons_validos:
-                 premio = r.get("Nombre del premio", "N/A")
-                 anio = r.get("Año", "N/A")
-                 ganador = r.get("Nombre del Ganador", "N/A")
-                 cargo = r.get("Cargo", "N/A")
+        for r in recons_validos:
+            premio = r.get("Nombre del premio", "N/A")
+            anio = r.get("Año", "N/A")
+            ganador = r.get("Nombre del Ganador", "N/A")
+            cargo = r.get("Cargo", "N/A")
                 
-        doc.add_paragraph(
+            doc.add_paragraph(
                 f" {premio} ({anio}): Otorgado a {ganador}, en su calidad de {cargo}.", 
                  style='List Bullet'
             )
@@ -626,6 +626,7 @@ if generar:
    #     file_name=f"PEP_{denom.replace(' ', '_')}.docx",
     #    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 #)
+
 
 
 
