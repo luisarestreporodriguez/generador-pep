e0126712b0c1958d5730c78e150e25accb656353
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-01-28 11:31:06 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -292,19 +292,18 @@ if generar:
         # PÁRRAFO 5: Reconocimientos
 recons_validos = [r for r in recon_data if r.get("Nombre del premio", "").strip()]
         
-if recons_validos:
-        doc.add_paragraph(
+    if recons_validos:
+            doc.add_paragraph(
                 f"El Programa de {denom} ha alcanzado importantes logros académicos e institucionales "
                 f"que evidencian su calidad y compromiso con la excelencia. Entre ellos se destacan:"
             )
-        for r in recons_validos:
-            premio = r.get("Nombre del premio", "N/A")
-            anio = r.get("Año", "N/A")
-            ganador = r.get("Nombre del Ganador", "N/A")
-            cargo = r.get("Cargo", "N/A")
-                
-        doc.add_paragraph(
-            f"• {premio} ({anio}): Otorgado a {ganador}, en su calidad de {cargo}.", 
+            for r in recons_validos:
+                premio = r.get("Nombre del premio", "N/A")
+                anio = r.get("Año", "N/A")
+                ganador = r.get("Nombre del Ganador", "N/A")
+                cargo = r.get("Cargo", "N/A")
+                doc.add_paragraph(
+            f" {premio} ({anio}): Otorgado a {ganador}, en su calidad de {cargo}.", 
             style='List Bullet')
 
         # Línea de tiempo
