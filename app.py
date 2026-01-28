3cc4c1ea0f2523133874a156013edd9ee82b5376
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-01-28 09:15:13 -0500

    App.py

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ if recons_validos:
 doc.add_heading('Línea de Tiempo del Programa', level=2)
 
 # 1. Función para extraer solo el año (busca 4 números seguidos)
- def limpiar_anio(texto):
+def limpiar_anio(texto):
     if not texto: return None
      match = re.search(r'(19|20)\d{2}', str(texto))
     return match.group(0) if match else None
@@ -626,6 +626,7 @@ if generar:
    #     file_name=f"PEP_{denom.replace(' ', '_')}.docx",
     #    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 #)
+
 
 
 
