0061031712efa335b798e381c44a990dab209d55
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-01-30 15:39:03 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -369,7 +369,7 @@ if planes_fec_lista:
                     f"y las demandas del entorno. Como resultado, se han realizado las modificaciones curriculares "
                     f"{txt_planes_lista}, aprobadas mediante el {txt_acuerdos_formateado}, respectivamente."
         )
-    doc.add_paragraph(texto_planes)
+    p_planes = doc.add_paragraph(texto_planes)
     p_planes.alignment = 3  # Justificado
     
         # PÁRRAFO 5: Reconocimientos
