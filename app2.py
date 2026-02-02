3daff190e6ae7fca17f45cfb5fef5997ad8f2f92
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-02 08:42:14 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -344,7 +344,7 @@ if generar:
 planes_nom = [n for n in [p1_nom, p2_nom, p3_nom] if n]
 planes_fec_lista = [f for f in [p1_fec, p2_fec, p3_fec] if f]
         
-if planes_fec_lista and planes_nom_lista:
+if planes_fec_lista and planes_nom:
             # A. Formatear nombres de planes (lo que antes era "lista")
             if len(planes_nom) > 1:
                 txt_planes_lista = ", ".join(planes_nom[:-1]) + f" y {planes_nom[-1]}"
