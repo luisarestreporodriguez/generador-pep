5de06a934bf09eb1d53d32317e2a979d20940879
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-02 08:39:57 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -363,8 +363,8 @@ if planes_fec_lista and planes_nom_lista:
                  f"y las demandas del entorno. Como resultado, se han realizado las modificaciones curriculares "
                  f"{lista}, aprobadas mediante el {txt_acuerdos_formateado}, respectivamente."
         )
-    p_planes = doc.add_paragraph(texto_planes)
-    p_planes.alignment = 3  # Justificado
+            p_planes = doc.add_paragraph(texto_planes)
+            p_planes.alignment = 3  # Justificado
     
         # PÁRRAFO 5: Reconocimientos
 recons_validos = [r for r in recon_data if r.get("Nombre del premio", "").strip()]
