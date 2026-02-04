c3393451360bf604d2d57da41298bb1184f02254
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-04 08:14:27 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -310,7 +310,7 @@ with st.form("pep_form"):
 st.subheader("2.2. Fundamentación Epistemológica")
 
 # Creamos pestañas para organizar los 3 párrafos y sus tablas
-    tab1, tab2, tab3 = st.tabs(["Párrafo 1: Naturaleza", "Párrafo 2: Dimensión Funcional", "Párrafo 3: Integración"])
+tab1, tab2, tab3 = st.tabs(["Párrafo 1: Naturaleza", "Párrafo 2: Dimensión Funcional", "Párrafo 3: Integración"])
 
    with tab1:
        st.markdown("### Bloque 1: Naturaleza del Programa")
