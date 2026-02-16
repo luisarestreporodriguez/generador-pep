312e1b1b10b8adc87a2ff3f5ad3d78449164bdc1
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-16 09:40:37 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -927,7 +927,7 @@ if generar:
     else:
                 txt_acuerdos_formateado = planes_fec_lista[0]
 
-            texto_planes = (
+    texto_planes = (
                  f"El plan de estudios del Programa de {denom} ha sido objeto de procesos periódicos de evaluación, "
                  f"con el fin de asegurar su pertinencia académica y su alineación con los avances tecnológicos "
                  f"y las demandas del entorno. Como resultado, se han realizado las modificaciones curriculares "
