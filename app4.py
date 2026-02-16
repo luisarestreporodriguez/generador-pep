89434732ab0c2734b3b394ffd7bcf2b6d413f1be
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-16 10:18:45 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -862,17 +862,17 @@ area_especifica = st.text_area("Demostrar la relevancia del programa en el conte
     
 
 #  LÓGICA DE GENERACIÓN DEL WORD 
-        if generar:
-            if not denom or not reg1:
-                    st.error("⚠️ Falta información obligatoria (Denominación o Registro Calificado).")
-            else:
-                    doc = Document()
-                    # Estilo base
-                    style = doc.styles['Normal']
-                    style.font.name = 'Arial'
-                    style.font.size = Pt(11)
-                        # 1.1 Historia del Programa
-                    doc.add_heading("1.1. Historia del Programa", level=1)
+            if generar:
+                    if not denom or not reg1:
+                            st.error("⚠️ Falta información obligatoria (Denominación o Registro Calificado).")
+                    else:
+                            doc = Document()
+                            # Estilo base
+                            style = doc.styles['Normal']
+                            style.font.name = 'Arial'
+                            style.font.size = Pt(11)
+                                # 1.1 Historia del Programa
+                            doc.add_heading("1.1. Historia del Programa", level=1)
         
         # PÁRRAFO 1. Datos creación
     texto_historia = (
