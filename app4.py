074c137fafd31f15472becb014ebeec2c696d99a
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-16 18:35:22 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -598,7 +598,7 @@ if "config_cap4" not in st.session_state:
         if st.session_state.get("input_comp_social"):
             st.success("✅ Texto copiado de 5.1.4. Competencias de Desempeño Profesional:")
         st.markdown(f"> {st.session_state.input_comp_social}")
-    else:
+            else:
         st.warning("⚠️ Primero completa la sección 5.1.4")
 
     with col2:
