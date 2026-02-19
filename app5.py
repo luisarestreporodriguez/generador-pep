dcf05cd1d571935b2583d256dc7648a2e3dc82c9
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-19 00:58:52 -0500

    Update app5.py

--- app5.py
+++ app5.py
@@ -1892,17 +1892,17 @@ if texto_final:
             p.alignment = 3  # Justificado
 
 
-#GUARDAR ARCHIVO
-bio = io.BytesIO()
-st.write("Llegué aquí")
-doc.save(bio)
-bio.seek(0)
-    
-st.success("✅ ¡Documento PEP generado!")
-st.download_button(
-            label="📥 Descargar Documento PEP en Word",
-            data=bio.getvalue(),
-            file_name=f"PEP_Modulo1_{denom.replace(' ', '_')}.docx",
-             mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-              )
+    #GUARDAR ARCHIVO
+    bio = io.BytesIO()
+    st.write("Llegué aquí")
+    doc.save(bio)
+    bio.seek(0)
+        
+    st.success("✅ ¡Documento PEP generado!")
+    st.download_button(
+                label="📥 Descargar Documento PEP en Word",
+                data=bio.getvalue(),
+                file_name=f"PEP_Modulo1_{denom.replace(' ', '_')}.docx",
+                 mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                  )
     
