7c7593897665c43213e47351023011184d7c4dc8
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-19 00:26:17 -0500

    Update app5.py

--- app5.py
+++ app5.py
@@ -1913,10 +1913,10 @@ bio = io.BytesIO()
 doc.save(bio)
 bio.seek(0)
 
-    st.success("✅ ¡Documento PEP generado!")
-    st.download_button(
-                label="📥 Descargar Documento PEP en Word",
-                data=bio.getvalue(),
-                file_name=f"PEP_Modulo1_{denom.replace(' ', '_')}.docx",
-                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-                  )
+st.success("✅ ¡Documento PEP generado!")
+st.download_button(
+            label="📥 Descargar Documento PEP en Word",
+            data=bio.getvalue(),
+            file_name=f"PEP_Modulo1_{denom.replace(' ', '_')}.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              )
