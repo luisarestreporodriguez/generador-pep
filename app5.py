0eecee4ce11cd470555835f81d05f85566369caf
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-02-23 11:51:05 -0500

    Update app5.py

--- app5.py
+++ app5.py
@@ -1491,10 +1491,10 @@ with st.form("pep_form"):
         }
                 
         # Recorremos el diccionario e insertamos cada una
-         for p_holder, k_word in areas_mapeo.items():
-            exito = insertar_tabla_automatica(doc, p_holder, k_word)
-            if not exito:
-            st.warning(f"⚠️ No se encontró una tabla para el área: {k_word}")
+             for p_holder, k_word in areas_mapeo.items():
+                exito = insertar_tabla_automatica(doc, p_holder, k_word)
+                if not exito:
+                st.warning(f"⚠️ No se encontró una tabla para el área: {k_word}")
 
 
     
