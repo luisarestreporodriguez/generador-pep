054a9bd1d9079f3ce7a0fcb52e8c048d0f3e6525
luisarestreporodriguez <luisa.restrepor@pascualbravo.edu.co>
2026-03-02 08:31:48 -0500

    Uncomment facultad and departamento in datos_portada

--- app5.py
+++ app5.py
@@ -2640,8 +2640,8 @@ if generar:
                         "{{DENOMINACION}}": denom.upper(),
                         "{{DENOMINACION2}}": denom.title(),
                         "{{SNIES}}": snies,
-                        #"{{facultad}}": facultad,      
-                        #"{{departamento}}": departamento,
+                        "{{facultad}}": facultad,      
+                        "{{departamento}}": departamento,
                 }
                 reemplazar_en_todo_el_doc(doc, datos_portada)        
                     
